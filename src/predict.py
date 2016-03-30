f2afee72ac89a84350b34c517cce1790675f3da5
c-cooper <cotiger3@gmail.com>
2016-03-30 19:43:25 -0400

    added prediction script

---
+++ src/predict.py
@@ -0,0 +1,22 @@
+"""                                                                              
+ Text-Machine Lab: MSA  
+
+ File Name : predict.py
+                                                                              
+ Creation Date : 30-03-2016
+                                                                              
+ Created By : Connor Cooper 
+                                                                              
+ Purpose : Predicts the positive/negative polarity of a movie review using
+           a pre-trained model
+"""
+
+# TODO: read in test data
+
+# TODO: lookup word vectors for test data (how do we handle unseen vectors?)
+
+# TODO: using word vectors, generate document vector
+
+# TODO: pass document vector to SVM classifier
+
+# TODO: process output (for now, printing to stdout with file name and classification)
